46c5475578ba1b95871f834aca738565a0262262
Hackerboy31 <shauryakumar0831@gmail.com>
2026-05-07 04:35:33 +0530

    add api in server link

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL ||
-    "https://teamtaskmanagerapp-production-1c74.up.railway.app",
+    "https://teamtaskmanagerapp-production-1c74.up.railway.app/api",
   headers: {
     "Content-Type": "application/json",
   },
